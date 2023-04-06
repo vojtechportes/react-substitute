eb3c387e6f79705b98ed85c3a7fbdfa8ff3f5386
Vojtěch Václav Porteš <info@justart.org>
2023-04-06 12:54:16 +0200

    feat: Added force replace option to allow substitution with ignoring the context object. That way placeholders are being processed and replaced in transform function.

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,13 @@
 import 'react-app-polyfill/ie11';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { FauxLink } from './FauxLink';
 import { Link } from './Link';
 import { substitute } from '../src/substitute';
 
 export default function App() {
   const text =
-    'Lorem ipsum dolor sit amet {user|123, John Doe}, {user|456, Jane Doe}';
+    'Lorem ipsum dolor sit amet {user|123, John Doe}, {user|456, Jane Doe} {token|789} {group|011} {group}';
 
   const substitutedText = substitute(text, {
     transform: (value, _placeholder, key, _modifier, context) => {
@@ -16,23 +17,29 @@ export default function App() {
         return `<0 to="/users/${userId}">${userName}</0>`;
       }
 
+      if (key === 'token') {
+        const [tokenId] = context;
+
+        return `<1>${key}: ${tokenId}</1>`;
+      }
+
+      const [identifier] = context;
+
+      if (identifier) {
+        return `<1>${identifier}</1>`;
+      }
+
       return value;
     },
-    context: {
-      user: '',
-    },
+    forceReplace: true,
     contextSeparator: '|',
     modifierSeparator: ':',
     contextType: 'list',
-    components: [<Link />],
+    components: [<Link />, <FauxLink />],
     returnReactElement: true,
   });
 
-  return (
-    <div className="App">
-      {substitutedText}
-    </div>
-  );
+  return <div className="App">{substitutedText}</div>;
 }
 
 ReactDOM.render(<App />, document.getElementById('root'));
